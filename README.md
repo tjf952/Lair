48d859bffa7ed15e27cfaf53ca97ef31fd1d074b
T.F <tjf952@gmail.com>
2025-01-17 00:07:18 -0800

    Main commit with the latest state

---
+++ README.md
@@ -0,0 +1,2 @@
+# checkmate.github.io
+A site for inviting my Groomsmen to my Discord server
---
+++ hidden_js/puzzl3.js
@@ -0,0 +1,27 @@
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
+
+// Detect if it's a touch device
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function getMousePosition(e) {
+  mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+  mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+  flashlight.style.setProperty("--Xpos", mouseX + "px");
+  flashlight.style.setProperty("--Ypos", mouseY + "px");
+}
+
+document.addEventListener("mousemove", getMousePosition);
+document.addEventListener("touchmove", getMousePosition);
+
+// One of four pieces?
+// 3. NzQgNjggMzMgNWYgNzMgMz
